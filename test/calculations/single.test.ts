62c07123bf4aa3bb6c88ec5f87558d8a65034f14
Samuel <st@kamae.pt>
2024-03-21 15:19:55 +0000

    added a test

--- test/calculations/single.test.ts
+++ test/calculations/single.test.ts
@@ -1,7 +1,8 @@
 import { describe, expect, it } from 'vitest'
 import { calculateSingle, calculateSingleElement } from '../../src/calculations/single'
 import type { DataElement, InputData } from '../../src/input'
-import type { OutputData } from '../../src/output'
+import type { OutputData, SingleOutput } from '../../src/output'
+import { calculateStatic } from '../../src/calculations/static'
 
 describe('element operations', () => {
   it('count', () => {
@@ -62,6 +63,19 @@ describe('element operations', () => {
 
     for (const { elements, expected } of data)
       expect(calculateSingleElement ('min', elements.map(e => ({ value: e } as DataElement)))).toBe(expected)
+  })
+
+  it('static', () => {
+    const data = [
+      { element: 1, expected: { type: 'single', value: 1 } as SingleOutput },
+      { element: 2, expected: { type: 'single', value: 2 } as SingleOutput },
+      { element: 3, expected: { type: 'single', value: 3 } as SingleOutput },
+      { element: 4, expected: { type: 'single', value: 4 } as SingleOutput },
+      { element: 5, expected: { type: 'single', value: 5 } as SingleOutput },
+    ]
+
+    for (const { element, expected } of data)
+      expect(calculateStatic({} as InputData, { type: 'static', value: element })).toStrictEqual(expected)
   })
 })
 
